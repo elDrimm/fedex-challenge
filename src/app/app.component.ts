4fdb0426414b835ceff329a17a0826b3467c703b
Wesley <wesley@softwarebastards.nl>
2022-08-26 15:17:05 +0200

    feat(styles): Adding base styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fed-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'fedex';
+
 }
